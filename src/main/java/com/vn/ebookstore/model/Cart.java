aedd3abf9b4fe25b682dac2831baa55541a03738
Phạm Quốc Tuấn <qtuan.ph@gmail.com>
2025-07-05 02:02:45 +0700

    push

--- src/main/java/com/vn/ebookstore/model/Cart.java
+++ src/main/java/com/vn/ebookstore/model/Cart.java
@@ -1,13 +1,22 @@
 package com.vn.ebookstore.model;
 
-import jakarta.persistence.*;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 @Entity
-@Table(name = "carts")
+@Table(name = "cart")
 public class Cart {
 
     @Id
